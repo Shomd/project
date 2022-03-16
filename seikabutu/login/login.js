8b1fa6b3cd596346999d4f4d4a498acdf6985eb2
Shomd <shm1113.vvvvvv@gmail.com>
2022-03-17 00:25:03 +0900

    second commit

--- seikabutu/login/login.js
+++
@@ -1,5 +0,0 @@
-function nextPage() {
-    const id = document.login_form.user_id.value //ID枠に入力された値（文字列）を定義
-    const pwd = document.login_form.password.value; //Password枠に入力された値（文字列）を定義
-    location.href = id + pwd + ".html"; //リンク先URLを作成
-}
--- seikabutu/login/login.php
+++ seikabutu/login/login.php
@@ -1,3 +1,30 @@
+<?php
+
+    $error = array();
+
+    if($_POST['memberID']){
+        if(!$_POST){
+            $error[] = "社員番号を入力してください";
+        } else if(strlen($_POST['memberID']) > 10) {
+            $error[] = "社員番号は１０文字以下にしてください";
+        } else if (!filter_var($_POST['memberID'], FILTER_VALIDATE_INT)){
+            $error[] = "社員番号またはパスワードが不正です";
+        }
+    }
+
+    if($_POST['memberPass']){
+        if(!$_POST){
+            $error[] = "パスワードを入力してください";
+        } else if(strlen($_POST['memberPass']) < 4 && strlen($_POST['memberPass']) > 10) {
+            $error[] = "パスワードは４文字以上８文字以下にしてください";
+        } else if (!filter_var($_POST['memberPass'], FILTER_VALIDATE_INT)){
+            $error[] = "社員番号またはパスワードが不正です";
+        }
+    }
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,38 +35,44 @@
     <title>login</title>
 </head>
 <body>
-<h1>ログイン</h1>
-    <!-- ログインフォーム -->
-    <div class = "content">
+    <header class = "header">
+        <h1>勤怠管理システム</h1>
+    </header>
 
-        <form class = "login_form" method = "post" name = "login_form">
-            <p class = "memberID" >社員番号</p>
-            <div>
-                <input class = "login" type = "text" name = "memberID" placeholder = "社員番号を入力">
-                <br>
-                <p class = "memberPass">password</p>
-            </div>
-            <div>
-                <input class = "login" type = "text" name = "memberPass" placeholder = "パスワードを入力">
-            </div>
-            <div>
-                <input class = "submit" type = "submit" value = "認証">
-            </div>
-            
-        </form>
+    <!-- ログインフォーム -->
+    <div class = "container">
+        <p class = "phrase">ログイン</p>
+            <?php
+                if($error){
+                    echo '<div class = "alert" role = "alert">';
+                    echo implode('<br>', $error);
+                    echo '</div>';
+                }
+            ?>
+        <div>
+            <form class = "login_form" method = "post" name = "login_form" autocomplete="off">
+                <p class = "member" >社員番号</p>
+                <div>
+                    <input class = "login" type = "text" name = "memberID" placeholder = "社員番号を入力">
+                    <br>
+                    <p class = "member">パスワード</p>
+                </div>
+                <div>
+                    <input class = "login" type = "text" name = "memberPass" placeholder = "パスワードを入力">
+                </div>
+                <div>
+                    <input class = "submit" type = "submit" value = "認証">
+                </div>
+                
+            </form>
+        </div>
+        
     </div>
 
-    <?php
-  
+<?php
 
-
-   
-
-    ?>
-
-
-
-
+?>
+    
 
 
     <script type="text/javascript" src="login.js"></script>
